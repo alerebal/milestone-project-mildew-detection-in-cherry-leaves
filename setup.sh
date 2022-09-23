afe0e2d38c0c9dcacff1f6b751ec9ce7adddca65
alerebal <ale.rebal@gmail.com>
2022-09-23 11:46:54 +0100

    Fix comming back to the first version of setup.sh
    The app is crushing now, so I will back to the first version

--- setup.sh
+++ setup.sh
@@ -1,9 +1,5 @@
 mkdir -p ~/.streamlit/
 echo "\
-base='dark'\n\
-primaryColor='#586e46'\n\
-textColor='#bbb'\n\
-secondaryBackgroundColor='#101c2c'\n\
 [server]\n\
 headless = true\n\
 port = $PORT\n\
