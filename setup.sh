4443b09a184ca5bce7912d13b01b5d3add0572aa
alerebal <ale.rebal@gmail.com>
2022-09-23 11:29:20 +0100

    Fix add new lines to setup.sh

--- setup.sh
+++ setup.sh
@@ -1,9 +1,9 @@
 mkdir -p ~/.streamlit/
 echo "\
-base='dark'
-primaryColor='#586e46'
-textColor='#bbb'
-secondaryBackgroundColor='#101c2c'
+base='dark'\n\
+primaryColor='#586e46'\n\
+textColor='#bbb'\n\
+secondaryBackgroundColor='#101c2c'\n\
 [server]\n\
 headless = true\n\
 port = $PORT\n\
