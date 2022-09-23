9ae73bab16e9356ef265dbf1b60f1533d800a93a
alerebal <ale.rebal@gmail.com>
2022-09-23 12:52:24 +0100

    Fix trying again to set theme in setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,9 @@
 mkdir -p ~/.streamlit/
-echo "\
+echo "[theme]\n\
+base='dark'\n\
+primaryColor='#586e46'\n\
+textColor=#bbb'\n\
+secondaryBackgroundColor='#101c2c'\n\
 [server]\n\
 headless = true\n\
 port = $PORT\n\
