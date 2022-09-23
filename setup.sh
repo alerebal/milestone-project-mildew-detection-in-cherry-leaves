d947603ceed83c249183336b277d4527a480f360
alerebal <ale.rebal@gmail.com>
2022-09-23 11:20:07 +0100

    Fix setup.sh to set the theme in heroku

--- setup.sh
+++ setup.sh
@@ -1,8 +1,12 @@
 mkdir -p ~/.streamlit/
 echo "\
+base='dark'
+primaryColor='#586e46'
+textColor='#bbb'
+secondaryBackgroundColor='#101c2c'
 [server]\n\
 headless = true\n\
 port = $PORT\n\
 enableCORS = false\n\
 \n\
-" > ~/.streamlit/config.toml
+" >> ~/.streamlit/config.toml
