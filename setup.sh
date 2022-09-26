b3f01287baeec567c5aff676370d70b14b0c8d82
alerebal <ale.rebal@gmail.com>
2022-09-26 08:08:52 +0100

    New attempt to fix theme
    Now I put the theme configuration below server configuration

--- setup.sh
+++ setup.sh
@@ -5,4 +5,10 @@ headless = true\n\
 port = $PORT\n\
 enableCORS = false\n\
 \n\
+[theme]\n\
+base='dark'\n\
+primaryColor='#586e46'\n\
+textColor='#bbb'\n\
+secondaryBackgroundColor='#101c2c'\n\
+\n\
 " > ~/.streamlit/config.toml
